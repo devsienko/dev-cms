61d84123c58303b81f40a80386061644270808d8
daniil <daniil_evsienko@mail.ru>
2020-02-17 21:36:44 +0400

    Add dictiionary attribute type (improved some UI components and logic)

--- DevCms/Areas/Admin/Controllers/DictionariesController.cs
+++ DevCms/Areas/Admin/Controllers/DictionariesController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using DevCms.Areas.Admin.Components;
 using DevCms.ContentTypes;
@@ -29,32 +30,39 @@ namespace DevCms.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-
                 var lastId = Dictionaries.Dicts.Max(d => d.Id);
                 var dictionary = new Dictionary
                 {
                     Id = lastId + 1,
-                    Name = model.Name
+                    Name = model.Name,
+                    Items = new List<DictionaryItem>()
                 };
                 Dictionaries.Dicts.Add(dictionary);
                 return View(nameof(Edit), new EditDictionaryDto
                 {
                     Id = dictionary.Id,
-                    Name = dictionary.Name
+                    Name = dictionary.Name,
+                    Items = dictionary.Items.Select(i => new EditDictionaryItemDto
+                    {
+                        Id = i.Id,
+                        Name = i.Name
+                    })
                 });
             }
             return View(model);
         }
 
-        public IActionResult Edit(int id)
+        public IActionResult Edit(int id, int? itemId)
         {
             if (id < 0)
+                return NotFound();
+            if (itemId.HasValue && itemId < 0)
                 return NotFound();
             var dictionary = Dictionaries.Dicts
                 .FirstOrDefault(t => t.Id == id);
             if (dictionary == null)
                 return NotFound();
-            return View(new EditDictionaryDto
+            var model = new EditDictionaryDto
             {
                 Id = dictionary.Id,
                 Name = dictionary.Name,
@@ -63,7 +71,15 @@ namespace DevCms.Areas.Admin.Controllers
                     Id = i.Id,
                     Name = i.Name
                 })
-            });
+            };
+            var isEditItemRequest = itemId.HasValue;
+            if (isEditItemRequest)
+            {
+                model.AddedOrEditedItem = model.Items.FirstOrDefault(at => at.Id == itemId);
+                if (model.AddedOrEditedItem == null)
+                    return NotFound();
+            }
+            return View(model);
         }
 
 
@@ -83,7 +99,7 @@ namespace DevCms.Areas.Admin.Controllers
                     return NotFound();
                 dictionary.Items.Add(new DictionaryItem
                 {
-                    Id = dictionary.Items.Max(i => i.Id) + 1,
+                    Id = dictionary.Items.DefaultIfEmpty(new DictionaryItem{ Id = 0 }).Max(i => i.Id) + 1,
                     Name = model.AddedOrEditedItem.Name
                 });
                 ModelState.Clear();
@@ -105,12 +121,21 @@ namespace DevCms.Areas.Admin.Controllers
                 if (dictionary == null)
                     return NotFound();
                 dictionary.Name = model.Name;
-
+                if (model.AddedOrEditedItem != null && model.AddedOrEditedItem.Id.HasValue)
+                {
+                    var editedAttr = dictionary.Items.First(at => at.Id == model.AddedOrEditedItem.Id);
+                    editedAttr.Name = model.AddedOrEditedItem.Name;
+                }
                 ModelState.Clear();
                 model = new EditDictionaryDto
                 {
                     Id = dictionary.Id,
-                    Name = dictionary.Name
+                    Name = dictionary.Name,
+                    Items = dictionary.Items.Select(i => new EditDictionaryItemDto
+                    {
+                        Id = i.Id,
+                        Name = i.Name
+                    })
                 };
             }
 
